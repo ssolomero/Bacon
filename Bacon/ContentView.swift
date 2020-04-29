bb2318effd709b1f498ac725160aa87918577b3a
Sharence Solomero <ssolomero@gmail.com>
2020-04-29 16:33:18 -0400

    Header & Profile Photo

--- Bacon/ContentView.swift
+++ Bacon/ContentView.swift
@@ -10,7 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            //Background
+            LinearGradient(gradient: Gradient(colors: [Color("Dark Blue"), Color("Gradient Green")]), startPoint: .topLeading, endPoint: .bottomLeading)
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .center) {
+                
+                //Logo & Menu
+                HStack(alignment: .center) {
+                    Spacer()
+                    Image("bacon_logo")
+                        .resizable()
+                        .frame(width: 70, height: 42, alignment: .center)
+                    Spacer()
+                    Image("Menu")
+                }
+                .padding(.top)
+                .padding(.leading, (70/2 + 20))
+                .padding(.trailing, 20)
+                .padding(.bottom, 25)
+            
+                //Greeting
+                Text("Good afternoon, Sharence!")
+                    .foregroundColor(.white)
+                    .font(Font.custom("Montserrat", size: 14))
+                    .padding(.bottom)
+
+                //Photo
+                Image("chance")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 75, height: 75, alignment: .center)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color("Gold"), lineWidth: 3))
+                
+
+                //Amount, bacon wallet
+                //Actions
+                //Your activity
+                //Incomplete
+                
+            }
+        }
     }
 }
 
@@ -19,3 +61,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
